f221801c3962e0b339f2210e85175b323544e687
Ashraf Hafiz <Ashraf.Hafiz@yahoo.com>
2022-04-16 23:45:23 +0200

    16042022-11:45PM

---
+++ resources/views/components/category.blade.php
@@ -0,0 +1,9 @@
+<div>
+    <a href="categories/{{ $category->slug }}" class="{{ $category->depth == 0 ? 'font-bold' : '' }}">{{ $category->title }}</a>
+
+    @foreach($category->children as $child)
+        <div class="ml-4">
+            <x-category :category="$child" />
+        </div>
+    @endforeach
+</div>
