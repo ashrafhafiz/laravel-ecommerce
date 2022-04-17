ad214789e0020e8668236e29b5e6dd1643800eb8
Ashraf Hafiz <Ashraf.Hafiz@yahoo.com>
2022-04-17 02:42:06 +0200

    17042022-02:30AM

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cknow\Money\Money;
 
 class Product extends Model
 {
     use HasFactory;
+
+    public function formattedPrice()
+    {
+        //return Money::USD($this->price);
+        return money($this->price);
+    }
+
+    public function variations()
+    {
+        return $this->hasMany(Variation::class);
+    }
 }
