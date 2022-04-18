2fc5b9a8d335a598f6c1d0d30e9974ac37be60d5
Ashraf Hafiz <ashraf.hafiz@yahoo.com>
2022-04-18 15:15:47 +0200

    18042022-03:15PM

--- app/Models/Variation.php
+++ app/Models/Variation.php
@@ -11,8 +11,39 @@ class Variation extends Model
     use HasFactory;
     use HasRecursiveRelationships;
 
+    public function formattedPrice()
+    {
+        //return Money::USD($this->price);
+        return money($this->price);
+    }
+
     public function product()
     {
         return $this->belongsTo(Product::class);
+    }
+
+    public function stocks()
+    {
+        return $this->hasMany(Stock::class);
+    }
+
+    public function stockCount()
+    {
+        return $this->descendantsAndSelf->sum(fn ($variation) => $variation->stocks->sum('amount'));
+    }
+
+    public function inStock()
+    {
+        return $this->stockCount() > 0;
+    }
+
+    public function outOfStock()
+    {
+        return !$this->inStock();
+    }
+
+    public function lowStock()
+    {
+        return !$this->outOfStock() && $this->stockCount() < 5;
     }
 }
